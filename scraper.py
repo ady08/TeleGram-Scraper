4301a283085ccbbc0060d7b9857acac21ad72f33
Nicoleus <52366737+ady08@users.noreply.github.com>
2019-12-07 10:25:43 +0700

    scraper.py

--- scraper.py
+++ scraper.py
@@ -12,12 +12,12 @@ cy="\033[1;36m"
 
 def banner():
     print(f"""
-{re}╔╦╗{cy}┌─┐┬  ┌─┐{re}╔═╗  ╔═╗{cy}┌─┐┬─┐┌─┐┌─┐┌─┐┬─┐
+{re}╔╦╗{cy}┌─┐┬  ┌─┐{re}╔═╗  ╔═╗{gr}┌─┐┬─┐┌─┐┌─┐┌─┐┬─┐
 {re} ║ {cy}├┤ │  ├┤ {re}║ ╦  ╚═╗{cy}│  ├┬┘├─┤├─┘├┤ ├┬┘
-{re} ╩ {cy}└─┘┴─┘└─┘{re}╚═╝  ╚═╝{cy}└─┘┴└─┴ ┴┴  └─┘┴└─
+{re} ╩ {cy}└─┘┴─┘└─┘{re}╚═╝  ╚═╝{gr}└─┘┴└─┴ ┴┴  └─┘┴└─
 
             version : 1.0
-        youtube.com/theunknon
+        youtube.com/ady sitorus ady
         """)
 
 cpass = configparser.RawConfigParser()
